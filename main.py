9a4530feaee89b1286600635176fe97cfadaa89a
Josafat Jimenez <josa221001@gmail.com>
2023-07-08 19:30:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
-from flask import Flask, jsonify
+from flask import Flask, request, render_template, send_file
+import base64
 import os
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return render_template('index.html')
 
+@app.route('/save', methods=['POST'])
+def save():
+    imageData = request.form.get('imageData')
+    imageType = request.form.get('imageType')
 
+    if imageData and imageType:
+        imageData = imageData.replace('data:image/png;base64,', '')
+        imageData = imageData.replace('data:image/jpeg;base64,', '')
+
+        imageBytes = base64.b64decode(imageData)
+
+        filename = 'drawing.' + imageType
+        with open(filename, 'wb') as f:
+            f.write(imageBytes)
+
+        return 'Imagen guardada con exito'
+
+    return 'Error al guardar la imagen'
+
+@app.route('/download/<filename>')
+def download(filename):
+    return send_file(filename, as_attachment=True)
+    
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
